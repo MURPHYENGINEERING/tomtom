fa97cf187cc3ac0671cf72647f11bace224705a7
James Whitehead Ii <jnwhiteh@gmail.com>
2007-03-30 03:49:04 +0000

    * Updated to Dongle-Beta1. * Initial release on WoWinterface.com

--- TomTom.lua
+++ TomTom.lua
@@ -1,23 +1,25 @@
-local L = {
+local L = setmetatable({
 	TOOLTIP_TITLE = "TomTom";
 	TOOLTIP_SUBTITLE = "Zone Coordinates";
 	TOOLTIP_LOCKED = "This window is locked in place.";
 	TOOLTIP_LEFTCLICK = "Left-click and drag to move this window.";
 	TOOLTIP_RIGHTCLICK = "Right-click to toggle the options panel.";
-}
+}, {__index=function(t,k) return k end})
 
-TomTom = DongleStub("Dongle-Beta0"):New("TomTom")
+TomTom = DongleStub("Dongle-Beta1"):New("TomTom")
 local DongleFrames = DongleStub("DongleFrames-1.0")
 local Astrolabe = DongleStub("Astrolabe-0.3")
 local profile
 
-function TomTom:Enable()
+function TomTom:Initialize()
 	self.defaults = {
 		profile = {
+			clearwaypoints = true,
 			show = true,
 			lock = false,
 			worldmap = true,
 			cursor = true,
+			tooltip = true,
 			alpha = 1,
 			notes = {
 			},
@@ -26,161 +28,253 @@ function TomTom:Enable()
 	
 	self.db = self:InitializeDB("TomTomDB", self.defaults)
 	profile = self.db.profile
-	
 	self:CreateSlashCommands()
+	self:CreateCoordWindows()
+	self:RegisterEvent("PLAYER_LEAVING_WORLD")
+	self:RegisterEvent("PLAYER_ENTERING_WORLD")
+	self:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+	self:RegisterEvent("WORLD_MAP_UPDATE")
 end
 
-local OnMouseDown = function(self,button)
-	if button == "LeftButton" and not profile.lock then
-		self:StartMoving()
-		self.isMoving = true
-	end
-end
-
-local OnMouseUp = function(self,button)
-	if self.isMoving then
-		self:StopMovingOrSizing()
-		self.isMoving = false
-	end
-end
-
-local OnEnter = function(self)
-	if profile.tooltip then
-		GameTooltip:SetDefaultAnchor(GameTooltip, self)
-		GameTooltip:SetText(L["TOOLTIP_TITLE"])
-		GameTooltip:AddLine(L["TOOLTIP_SUBTITLE"])
-		if profile.lock then
-			GameTooltip:AddLine(L["TOOLTIP_LOCKED"])
-		else
-			GameTooltip:AddLine(L["TOOLTIP_LEFTCLICK"])
+function TomTom:CreateCoordWindows()
+	-- Create the draggable frame, as well as the world map coords
+	local function OnMouseDown(self,button)
+		if button == "LeftButton" and not profile.lock then
+			self:StartMoving()
+			self.isMoving = true
 		end
-		GameTooltipTextLeft1:SetTextColor(1,1,1);
-		GameTooltipTextLeft2:SetTextColor(1,1,1);
-		GameTooltip:Show()
 	end
-end
 
-local OnLeave = function(self)
-	GameTooltip:Hide();
-end
+	local function OnMouseUp(self,button)
+		if self.isMoving then
+			self:StopMovingOrSizing()
+			self.isMoving = false
+		end
+	end
 
-local OnUpdate = function(self, elapsed)
-	local c,z,x,y = Astrolabe:GetCurrentPlayerPosition()
+	local function OnEnter(self)
+		if profile.tooltip then
+			GameTooltip:SetOwner(self, "ANCHOR_CURSOR")
+			GameTooltip:SetText(L["TOOLTIP_TITLE"])
+			GameTooltip:AddLine(L["TOOLTIP_SUBTITLE"])
+			if profile.lock then
+				GameTooltip:AddLine(L["TOOLTIP_LOCKED"])
+			else
+				GameTooltip:AddLine(L["TOOLTIP_LEFTCLICK"])
+			end
+			GameTooltipTextLeft1:SetTextColor(1,1,1);
+			GameTooltipTextLeft2:SetTextColor(1,1,1);
+			GameTooltip:Show()
+		end
+	end
+
+	local function OnLeave(self)
+		GameTooltip:Hide();
+	end
 	
-	if not x or not y then
-		text = "---"
+	local function CoordFrame_OnUpdate(self, elapsed)		
+		local c,z,x,y = Astrolabe:GetCurrentPlayerPosition()
+		local text
+		if not x or not y then
+			text = "---"
+		else
+			self.Text:SetText(string.format("%.2f, %.2f", x*100, y*100))
+		end
+	end
+
+	local function WorldMap_OnUpdate(self, elapsed)
+		local c,z,x,y = Astrolabe:GetCurrentPlayerPosition()
+
+		-- Coordinate calculation code taken from CT_MapMod
+		local cX, cY = GetCursorPosition()
+		local ceX, ceY = WorldMapFrame:GetCenter();
+		local wmfw, wmfh = WorldMapButton:GetWidth(), WorldMapButton:GetHeight();
+		
+		cX = ( ( ( cX / WorldMapFrame:GetScale() ) - ( ceX - wmfw / 2 ) ) / wmfw + 22/10000 );
+		cY = ( ( ( ( ceY + wmfh / 2 ) - ( cY / WorldMapFrame:GetScale() ) ) / wmfh ) - 262/10000 );
+		
+		if not x or not y then
+			self.Player:SetText("Player: ---")
+		else
+			self.Player:SetText(string.format("Player: %.2f, %.2f", x*100, y*100))
+		end
+		
+		if not cX or not cY then
+			self.Cursor:SetText("Cursor: ---")
+		else
+			self.Cursor:SetText(string.format("Cursor: %.2f, %.2f", cX*100, cY*100))
+		end
+	end  
+	
+	-- Create TomTomFrame, which is the coordinate display
+	DongleFrames:Create("n=TomTomFrame#p=UIParent#size=100,32#toplevel#strata=HIGH#mouse#movable#clamp", "CENTER", 0, 0)
+	TomTomFrame.Text = DongleFrames:Create("p=TomTomFrame#t=FontString#inh=GameFontNormal", "CENTER", 0, 0)
+	TomTomFrame:SetBackdrop({
+		bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
+		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+		edgeSize = 16,
+		insets = {left = 4, right = 4, top = 4, bottom = 4},
+	})
+	TomTomFrame:SetBackdropColor(0,0,0,0.4)
+	TomTomFrame:SetBackdropBorderColor(1,0.8,0,0.8)
+	TomTomFrame:SetScript("OnMouseDown", OnMouseDown)
+	TomTomFrame:SetScript("OnMouseUp", OnMouseUp)
+	TomTomFrame:SetScript("OnHide", OnMouseUp)
+	TomTomFrame:SetScript("OnEnter", OnEnter)
+	TomTomFrame:SetScript("OnLeave", OnLeave)
+	TomTomFrame:SetScript("OnUpdate", CoordFrame_OnUpdate)
+		
+	-- Create TomTomWorldFrame, which is anchored to the center of the WorldMap
+	DongleFrames:Create("n=TomTomWorldFrame#p=WorldMapFrame")
+	TomTomWorldFrame.Player = DongleFrames:Create("p=TomTomWorldFrame#t=FontString#inh=GameFontHighlightSmall", "BOTTOM", WorldMapPositioningGuide, "BOTTOM", -100, 11)
+	TomTomWorldFrame.Cursor = DongleFrames:Create("p=TomTomWorldFrame#t=FontString#inh=GameFontHighlightSmall", "BOTTOM", WorldMapPositioningGuide, "BOTTOM", 100, 11)		
+	TomTomWorldFrame:SetScript("OnUpdate", WorldMap_OnUpdate)
+	
+	self.frame = CreateFrame("Frame")
+end
+
+local function MinimapIcon_OnEnter(self)
+	GameTooltip:SetOwner(self, "ANCHOR_CURSOR")
+	if self.label then
+		GameTooltip:SetText("TomTom: " .. self.label .. "\n")
 	else
-		text = string.format("%.2f, %.2f", x*100, y*100)
+		GameTooltip:SetText("TomTom Waypoint\n")
 	end
-	
-	self.Text:SetText(text)
-end  
 
-local MinimapSize = {
-	indoor = {
-		[0] = 300, -- scale
-		[1] = 240, -- 1.25
-		[2] = 180, -- 5/3
-		[3] = 120, -- 2.5
-		[4] = 80,  -- 3.75
-		[5] = 50,  -- 6
-	},
-	outdoor = {
-		[0] = 466 + 2/3, -- scale
-		[1] = 400,       -- 7/6
-		[2] = 333 + 1/3, -- 1.4
-		[3] = 266 + 2/6, -- 1.75
-		[4] = 200,       -- 7/3
-		[5] = 133 + 1/3, -- 3.5
-	},
-}
+	local dist,x,y = Astrolabe:GetDistanceToIcon(self)
+
+	GameTooltip:AddLine(self.coord, 1, 1, 1)
+	GameTooltip:AddLine(("%s yards away"):format(math.floor(dist)), 1, 1 ,1)
+	GameTooltip:AddLine(self.zone, 0.7, 0.7, 0.7)
+	GameTooltip:Show()
+end
+
+local function MinimapIcon_OnLeave(self)
+	GameTooltip:Hide()
+end
 
 local halfpi = math.pi / 2
-
-local function updateAngle(icon)
+local function MinimapIcon_UpdateArrow(self, elapsed)
+	local icon = self.parent
 	local angle = Astrolabe:GetDirectionToIcon(icon)
 
 	local x = .03875* math.cos(angle + halfpi) + 0.04875 
 	local y = .03875* math.sin(angle + halfpi) + 0.04875 
-	icon.arrow:SetPosition(x,y,0)
-	icon.arrow:SetFacing(angle)	
+	self:SetPosition(x,y,0)
+	self:SetFacing(angle)	
 end
 
-local function iconOnUpdate(icon, elapsed)
-	if not icon.arrow then return end
+local function MinimapIcon_OnUpdate(self, elapsed)
+	local edge = Astrolabe:IsIconOnEdge(self)
+	local dot = self.dot:IsShown()
+	local arrow = self.arrow:IsShown()
 
-	local edge = Astrolabe:IsIconOnEdge(icon)
+	if edge and not arrow then
+		self.arrow:Show()
+		self.dot:Hide()
+		icon.edge = true
+	elseif not edge and not dot then
+		self.dot:Show()
+		self.arrow:Hide()
+		icon.edge = false
+	end
 
-	if edge then
-		icon.arrow:Show()
-		icon.icon:Hide()
-	elseif icon.arrow:IsVisible() then
-		icon.arrow:Hide()
-		icon.icon:Show()
+	local dist,x,y = Astrolabe:GetDistanceToIcon(self)
+	if dist < 11 and profile.clearwaypoints then
+		-- Clear this waypoint
+		Astrolabe:RemoveIconFromMinimap(self)
+		TomTom:PrintF("You have arrived at your location (%s)", self.coord)
 	end
 end
 
-function TomTom:GetMinimapIcon(desc)
-	self.mmicons = self.mmicons or {}
-
-	local icon = table.remove(self.mmicons)
-	if not icon then
-		icon = CreateFrame("Button", nil, Minimap)
-		icon:SetHeight(12)
-		icon:SetWidth(12)
-
-		local texture = icon:CreateTexture()
-		texture:SetTexture("Interface\\Minimap\\ObjectIcons")
-		texture:SetTexCoord(0.5, 0.75, 0, 0.25)
-		texture:SetAllPoints()
-		icon.icon = texture
-		icon:SetScript("OnUpdate", iconOnUpdate)
-
-		local frame = CreateFrame("Model", nil, Minimap)
-		frame:SetHeight(140.8)
-		frame:SetWidth(140.8)
-		frame:SetPoint("CENTER", Minimap, "CENTER", 0, 0)
-		frame:SetModel("Interface\\Minimap\\Rotating-MinimapArrow.mdx")
-		frame:SetFogColor(0.9999977946281433,0.9999977946281433,0.9999977946281433,0.9999977946281433)
-		frame:SetFogFar(1)
-		frame:SetFogNear(0)
-		frame:SetLight(0,1,0,0,0,1,1,1,1,1,1,1,1)
-		frame:SetModelScale(.600000023841879)
-		icon.arrow = frame
-		local total = 0
-		frame:SetScript("OnEnter", function() TomTom:Print("enter") end)
-		frame:SetScript("OnUpdate", function(self, elapsed)
-			-- Update arrow direction here
-			updateAngle(icon)
-		end)
-    
-		table.insert(self.points, icon)
-		return icon
-	else
-		return icon
+function TomTom:CreateMinimapIcon(label, x, y)
+	if not self.minimapIcons then
+		self.minimapIcons = {}
 	end
+
+	-- Return one from the frame pool, if possible
+	local icon = table.remove(self.minimapIcons)
+	if icon then
+		icon.label = label
+		icon.coord = string.format("%5.2f, %5.2f", x, y)
+		return icon 
+	end
+	
+	-- Create a new icon with arrow
+	-- icon.dot is the minimap dot texture
+	-- icon.arrow is the model used on the edge of the map
+	-- icon.label will contain the mouseover label
+	-- icon.coord will contain the text of the coordinates
+	icon = CreateFrame("Button", nil, Minimap)
+	icon:SetHeight(12)
+	icon:SetWidth(12)
+
+	icon.label = label
+	icon.coord = string.format("%5.2f, %5.2f", x, y)
+
+	local texture = icon:CreateTexture()
+	texture:SetTexture("Interface\\Minimap\\ObjectIcons")
+	texture:SetTexCoord(0.5, 0.75, 0, 0.25)
+	texture:SetAllPoints()
+	icon.dot = texture
+	icon:SetScript("OnEnter", MinimapIcon_OnEnter)
+	icon:SetScript("OnLeave", MinimapIcon_OnLeave)
+	icon:SetScript("OnUpdate", MinimapIcon_OnUpdate)
+
+	local model = CreateFrame("Model", nil, icon)
+	model:SetHeight(140.8)
+	model:SetWidth(140.8)
+	model:SetPoint("CENTER", Minimap, "CENTER", 0, 0)
+	model:SetModel("Interface\\Minimap\\Rotating-MinimapArrow.mdx")
+	model:SetFogColor(0.9999977946281433,0.9999977946281433,0.9999977946281433,0.9999977946281433)
+	model:SetFogFar(1)
+	model:SetFogNear(0)
+	model:SetLight(0,1,0,0,0,1,1,1,1,1,1,1,1)
+	model:SetModelScale(.600000023841879)
+	model.parent = icon
+	icon.arrow = model
+	model:SetScript("OnUpdate", MinimapIcon_UpdateArrow)
+	model:Hide()
+
+	return icon
 end
 
-function TomTom:GetWorldMapIcon()
-	self.wmicons = self.wmicons or {}
+local function WorldMapIcon_OnEnter(self)
+	TomTom:Print(self.coord)
+end
 
-	local icon = table.remove(self.wmicons)
-	if not icon then
-		icon = CreateFrame("Button", nil, WorldMapFrame)
-		icon:SetHeight(12)
-		icon:SetWidth(12)
-		local texture = icon:CreateTexture()
-		texture:SetTexture("Interface\\Minimap\\ObjectIcons")
-		texture:SetTexCoord(0.5, 0.75, 0, 0.25)
-		texture:SetAllPoints()
-		icon.icon = texture
-		icon:SetScript("OnUpdate", iconOnUpdate)
-
-		--table.insert(self.points, icon)
-		return icon
-	else
-		return icon
+function TomTom:CreateWorldMapIcon(label, x, y)
+	if not self.worldmapIcons then
+		self.worldmapIcons = {}
 	end
+
+	-- Return one from the frame pool, if possible
+	local icon = table.remove(self.worldmapIcons)
+	if icon then
+		icon.label = label
+		icon.coord = string.format("%5.2f, %5.2f", x, y)
+		return icon 
+	end
+	
+	-- Create a new icon with arrow
+	-- icon.dot is the minimap dot texture
+	-- icon.label will contain the mouseover label
+	-- icon.coord will contain the text of the coordinates
+	icon = CreateFrame("Button", nil, WorldMapDetailFrame)
+	icon:SetHeight(12)
+	icon:SetWidth(12)
+
+	icon.label = label
+	icon.coord = string.format("%5.2f, %5.2f", x, y)
+
+	local texture = icon:CreateTexture()
+	texture:SetTexture("Interface\\Minimap\\ObjectIcons")
+	texture:SetTexCoord(0.5, 0.75, 0, 0.25)
+	texture:SetAllPoints()
+	icon.dot = texture
+	icon:SetScript("OnEnter", WorldMapIcon_OnEnter)
+
+	return icon
 end
 
 function TomTom:CreateSlashCommands()
@@ -204,106 +298,121 @@ function TomTom:CreateSlashCommands()
 		self:PrintF("The coordinate display has been %s.", profile.lock and "locked" or "unlocked")
 	end
 	
-	self.cmd:RegisterSlashHandler("Show/Hide the coordinate display", "^(coord)$", ToggleDisplay)
-	self.cmd:RegisterSlashHandler("Show/Hide the world map coordinate display", "^(mapcoord)$", ToggleDisplay)
-	self.cmd:RegisterSlashHandler("Lock/Unlock the coordinate display", "^lock$", LockDisplay)
+	self.cmd:RegisterSlashHandler("coord - Show/Hide the coordinate display", "^(coord)$", ToggleDisplay)
+	self.cmd:RegisterSlashHandler("mapcoord - Show/Hide the world map coordinate display", "^(mapcoord)$", ToggleDisplay)
+	self.cmd:RegisterSlashHandler("lock - Lock/Unlock the coordinate display", "^lock$", LockDisplay)
 
 	-- Waypoint placement slash commands
 	self.cmd_way = self:InitializeSlashCommand("TomTom - Waypoints", "TOMTOM_WAY", "way")
-	
-	local total = 0
-	local Way_Update = function(frame,elapsed)
-		if total >= 0.2 then
-			total = total + elapsed
-			return
-		else
-			total = 0
-			local c,z,x,y = Astrolabe:GetCurrentPlayerPosition()
-			local zone = self.points and self.points[c] and self.points[c][z]
-			if not zone then return end
-
-			for idx,entry in ipairs(zone) do
-				-- Bail out if we're too high
-				local range = .001
-				local xeq = x <= entry.x + range and x >= entry.x - range
-				local yeq = y <= entry.y + range and y >= entry.y - range
-				if xeq and yeq then
-					self:PrintF("You have reached your destination at %.2f, %.2f.", x * 100 , y * 100)
-					Astrolabe:RemoveIconFromMinimap(entry.icon)
-					table.insert(self.mmicons, icon)
-					table.remove(zone, idx)
-					frame:SetScript("OnUpdate", nil)
-				end
-			end
-		end
-	end
 	
 	local sortFunc = function(a,b) 
 		if a.x == b.x then return a.y < b.y else return a.x < b.x end
 	end
 	
 	local Way_Set = function(x,y,desc)
-		if not self.points then self.points = {} end
+		if not self.m_points then self.m_points = {} end
+		if not self.w_points then self.w_points = {} end
+
+		local oc,oz = Astrolabe:GetCurrentPlayerPosition()
+		SetMapToCurrentZone()
+		local c,z = Astrolabe:GetCurrentPlayerPosition()
+		SetMapZoom(oc,oz)
+						
+		local m_icon = self:CreateMinimapIcon(desc, x, y)
+		local w_icon = self:CreateWorldMapIcon(desc, x, y)
+		--local c,z = Astrolabe:GetCurrentPlayerPosition()
 
 		x = x / 100
 		y = y / 100
-						
-		local icon = self:GetMinimapIcon(desc)
-		local c,z = Astrolabe:GetCurrentPlayerPosition()		
-		Astrolabe:PlaceIconOnMinimap(icon, c, z, x, y)
-		local icon = self:GetWorldMapIcon(desc)
-		Astrolabe:PlaceIconOnWorldMap(WorldMapFrame, icon, c, z, x, y)
 
-		self:PrintF("Setting a waypoint at %.2f, %.2f.", x * 100, y * 100)
-
-		self.points[c] = self.points[c] or {}
-		self.points[c][z] = self.points[c][z] or {}
+		Astrolabe:PlaceIconOnMinimap(m_icon, c, z, x, y)
+		Astrolabe:PlaceIconOnWorldMap(WorldMapDetailFrame, w_icon, c, z, x, y)
 		
-		table.insert(self.points[c][z], {["x"] = x, ["y"] = y, ["icon"] = icon})
-		table.sort(self.points[c][z], sortFunc)
-		self.frame:SetScript("OnUpdate", Way_Update)
+		w_icon:Show()
+
+		local zone = select(z, GetMapZones(c))
+		m_icon.zone = zone
+		w_icon.zone = zone
+
+		self:PrintF("Setting a waypoint at %.2f, %.2f in %s.", x * 100, y * 100, zone)
+
+		self.m_points[c] = self.m_points[c] or {}
+		self.m_points[c][z] = self.m_points[c][z] or {}
+		self.m_points.current = self.m_points[c][z]
+		
+		table.insert(self.m_points[c][z], {["x"] = x, ["y"] = y, ["icon"] = m_icon})
+		table.sort(self.m_points[c][z], sortFunc)
+
+		table.insert(self.w_points, {["c"] = c, ["z"] = z, ["x"] = x, ["y"] = y, ["icon"] = w_icon})
 	end
 	
 	local Way_Reset = function()
-		if #self.points == 0 then
+		if #self.m_points == 0 then
 			self:Print("There are no waypoints to remove.")
 			return
 		end
-		
-		for idx,icon in ipairs(self.points) do
-			Astrolabe:RemoveIconFromMinimap(icon)
-			icon:Hide()
+
+		if self.m_points then
+			for cont,ztbl in pairs(self.m_points) do
+				for zone,ztbl in pairs(ztbl) do
+					for idx,entry in pairs(ztbl) do
+						Astrolabe:RemoveIconFromMinimap(entry.icon)
+						table.insert(self.minimapIcons, entry.icon)
+						ztbl[idx] = nil
+					end
+				end
+			end
 		end
+
+		if self.w_points then
+			for k,v in ipairs(self.w_points) do
+				local icon = v.icon
+				icon:Hide()
+				self.w_points[k] = nil
+				table.insert(self.worldmapIcons, icon)
+			end
+		end
+		
 		self:Print("All waypoints have been removed.")
 	end
 		
-	self.cmd_way:RegisterSlashHandler("Remove all current waypoints", "^reset$", Way_Reset)
-	self.cmd_way:RegisterSlashHandler("Add a new waypoint with optional note", "^(%d*%.?%d*)[%s]+(%d*%.?%d*)[%s]*(.*)", Way_Set)
+	self.cmd_way:RegisterSlashHandler("reset - Remove all current waypoints", "^reset$", Way_Reset)
+	self.cmd_way:RegisterSlashHandler("<xx.xx> <yy.yy> [<desc>] - Add a new waypoint with optional note", "^(%d*%.?%d*)[%s]+(%d*%.?%d*)%s*(.*)", Way_Set)
 end
 
-function TomTom:CreateFrames()
-	DongleFrames:Create("n=TomTomFrame#p=UIParent#size=100,32#toplevel#strata=HIGH#mouse#movable#clamp", "CENTER", 0, 0)
-	TomTomFrame.Text = DongleFrames:Create("p=TomTomFrame#t=FontString#inh=GameFontNormal", "CENTER", 0, 0)
-	TomTomFrame:SetBackdrop({
-		bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
-		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
-		edgeSize = 16,
-		insets = {left = 4, right = 4, top = 4, bottom = 4},
-	})
-	TomTomFrame:SetBackdropColor(0,0,0,0.4)
-	TomTomFrame:SetBackdropBorderColor(1,0.8,0,0.8)
-	TomTomFrame:SetScript("OnMouseDown", OnMouseDown)
-	TomTomFrame:SetScript("OnMouseUp", OnMouseUp)
-	TomTomFrame:SetScript("OnHide", OnMouseUp)
-	TomTomFrame:SetScript("OnEnter", OnEnter)
-	TomTomFrame:SetScript("OnLeave", OnLeave)
-	TomTomFrame:SetScript("OnUpdate", OnUpdate)
-		
-	DongleFrames:Create("n=TomTomWorldFrame#p=WorldMapFrame")
-	TomTomWorldFrame.Text = DongleFrames:Create("p=TomTomWorldFrame#t=FontString#inh=GameFontHighlightSmall", "BOTTOM", WorldMapPositioningGuide, "BOTTOM", 0, 11)		
-	TomTomWorldFrame:SetScript("OnUpdate", OnUpdate)
-	
-	self.frame = CreateFrame("Frame")
+function TomTom:ZONE_CHANGED_NEW_AREA()
+	-- This could clear the minimap, but I won't.. cause.. I don't like that
+	-- Not sure what to do here
+	if true then return end
 end
 
-TomTom:CreateFrames()
+function TomTom:PLAYER_ENTERING_WORLD()
+	local oc,oz = Astrolabe:GetCurrentPlayerPosition()
+	SetMapToCurrentZone()
+	local c,z,x,y = Astrolabe:GetCurrentPlayerPosition()
+	SetMapZoom(oc,oz)
+
+	if self.m_points and self.m_points[c] then
+		for zone,v in pairs(self.m_points[c]) do
+			for idx,entry in pairs(v) do
+				Astrolabe:PlaceIconOnMinimap(entry.icon, c, zone, entry.x, entry.y)
+			end
+		end
+	end
+end
+
+function TomTom:WORLD_MAP_UPDATE()
+	if not self.w_points then return end
+
+	local c = GetCurrentMapContinent()
+
+	for idx,entry in ipairs(self.w_points) do
+		local icon = entry.icon
+		local x,y = Astrolabe:PlaceIconOnWorldMap(WorldMapDetailFrame, icon, entry.c, entry.z, entry.x, entry.y)
+		if (x and y and (0 < x and x <= 1) and (0 < y and y <= 1)) then 
+			icon:Show()
+		else
+			icon:Hide()
+		end
+	end
+end
