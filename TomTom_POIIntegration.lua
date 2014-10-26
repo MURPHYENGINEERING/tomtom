df361a10b0f52167ca5e75090d03208f1aa66f1e
Jim Whitehead <jnwhiteh@gmail.com>
2014-10-26 10:55:29 +0100

    Attempted fix for issue in TomTom_POIIntegration
    There may still be an issue with floors, please provide clear and direct
    bug reports if you're able to reproduce a Lua error.

--- TomTom_POIIntegration.lua
+++ TomTom_POIIntegration.lua
@@ -36,7 +36,7 @@ local function ObjectivesChanged()
 
     local map, floor = GetCurrentMapAreaID()
     local floors = astrolabe:GetNumFloors(map)
-    floor = floor and floor or (floors == 0 and 0 or 1)
+    floor = (floors == 0 and 0 or 1)
 
     local px, py = GetPlayerMapPosition("player")
 
